ea001e774cda7d8d122a06caa6dddb634c0f9703
DanielQnav <daniel.quintana1984@gmail.com>
2022-08-28 23:01:05 -0400

    dia no puede quedar en 0

--- app.js
+++ app.js
@@ -8,15 +8,23 @@ function edad(){
     var y2 = date.getFullYear();
     var month =[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-        if (d1 > d2){
+        if (d1 > d2 || d1 != 00 || d1 < 31){
             d2 = d2 + month[m2 - 1];
             m2 = m2 - 1;
         }
+        else{
+            document.write("error");
+        }
 
-        if (m1>m2){
+        if (m1 > m2 || m1 != 00){
             m2 = m2 + 12;
             y2 = y2 - 1;
         }
+        else{
+            document.write("error");
+        }
+        
+
         var d = d2 - d1;
         var m = m2 - m1;
         var y = y2 - y1;
@@ -24,6 +32,7 @@ function edad(){
         
         document.getElementById("edad").innerHTML =
             `Tu edad es ${y} Años ${m} meses ${d} días`;
+        
         
         
     
