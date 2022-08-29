b753087d1f8dcc8fedc332ebe7e4ad9c5f7a27f2
DanielQnav <daniel.quintana1984@gmail.com>
2022-08-28 23:12:16 -0400

    se vuelve al orginal por error en el calculo

--- app.js
+++ app.js
@@ -8,21 +8,17 @@ function edad(){
     var y2 = date.getFullYear();
     var month =[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-        if (d1 > d2 || d1 != 00 || d1 < 31){
+        if (d1 > d2){
             d2 = d2 + month[m2 - 1];
             m2 = m2 - 1;
         }
-        else{
-            document.write("error");
-        }
+       
 
-        if (m1 > m2 || m1 != 00){
+        if (m1 > m2){
             m2 = m2 + 12;
             y2 = y2 - 1;
         }
-        else{
-            document.write("error");
-        }
+       
         
 
         var d = d2 - d1;
